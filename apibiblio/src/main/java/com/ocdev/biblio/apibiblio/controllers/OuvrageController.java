2c03cecdb7d8fe907d7ee72e2df6c8ee14f15fb9
Christophe <christophe.orsini@gmail.com>
2020-07-08 13:07:28 +0200

    correction controllers

--- apibiblio/src/main/java/com/ocdev/biblio/apibiblio/controllers/OuvrageController.java
+++ apibiblio/src/main/java/com/ocdev/biblio/apibiblio/controllers/OuvrageController.java
@@ -1,20 +1,20 @@
 package com.ocdev.biblio.apibiblio.controllers;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.ocdev.biblio.apibiblio.assemblers.IAssembler;
-import com.ocdev.biblio.apibiblio.dto.OuvrageDetail;
-import com.ocdev.biblio.apibiblio.entities.Ouvrage;
-import com.ocdev.biblio.apibiblio.exceptions.NullOrEmptyArgumentException;
-import com.ocdev.biblio.apibiblio.exceptions.RequiredAndNotFoundException;
+import com.ocdev.biblio.apibiblio.dto.OuvrageDto;
+import com.ocdev.biblio.apibiblio.errors.AlreadyExistsException;
+import com.ocdev.biblio.apibiblio.errors.EntityNotFoundException;
 import com.ocdev.biblio.apibiblio.services.OuvrageService;
 
 @RestController
@@ -22,57 +22,41 @@ import com.ocdev.biblio.apibiblio.services.OuvrageService;
 public class OuvrageController
 {
 	@Autowired private OuvrageService ouvrageService;
-	@Autowired private IAssembler<Ouvrage, OuvrageDetail> ouvrageAssembler;
 	
 	@PostMapping(value ="/ouvrages")
-	public ResponseEntity<OuvrageDetail> createOuvrage(@RequestBody final OuvrageDetail ouvrageDetail)
+	public ResponseEntity<OuvrageDto> ajouterOuvrage(@RequestBody final OuvrageDto ouvrageDto)
 	{
-		OuvrageDetail newOuvrage = null;
-		
+		OuvrageDto result = null;
 		try
 		{
-			Ouvrage ouvrage = ouvrageService.ajouterOuChanger(ouvrageDetail.getTitre(), ouvrageDetail.getResume(),
-					ouvrageDetail.getAuteur(), ouvrageDetail.getAnneeParution(), ouvrageDetail.getTheme(), 0);
-			newOuvrage = ouvrageAssembler.createDto(ouvrage);
+			result = ouvrageService.creer(ouvrageDto);
 		}
-		catch (NullOrEmptyArgumentException e)
+		catch (AlreadyExistsException e)
 		{
-			return new ResponseEntity<OuvrageDetail>(HttpStatus.NO_CONTENT);
+			return new ResponseEntity<OuvrageDto>(HttpStatus.NO_CONTENT);
 		}
 		
-		return new ResponseEntity<OuvrageDetail>(newOuvrage, HttpStatus.CREATED);
-	}
-	
-	@PutMapping(value ="/ouvrages")
-	public ResponseEntity<OuvrageDetail> updateOuvrage(@RequestBody final OuvrageDetail ouvrageDetail)
-	{
-		OuvrageDetail newOuvrage = null;
-		
-		try
-		{
-			Ouvrage ouvrage = ouvrageService.ajouterOuChanger(ouvrageDetail.getTitre(), ouvrageDetail.getResume(),
-					ouvrageDetail.getAuteur(), ouvrageDetail.getAnneeParution(), ouvrageDetail.getTheme(), ouvrageDetail.getNbreExemplaire());
-			newOuvrage = ouvrageAssembler.createDto(ouvrage);
-		}
-		catch (NullOrEmptyArgumentException e)
-		{
-			return new ResponseEntity<OuvrageDetail>(HttpStatus.NOT_FOUND);
-		}
-		
-		return new ResponseEntity<OuvrageDetail>(newOuvrage, HttpStatus.OK);
+		return new ResponseEntity<OuvrageDto>(result, HttpStatus.CREATED);
 	}
 	
 	@GetMapping(value = "/ouvrages/{ouvrageId}")
-	public ResponseEntity<OuvrageDetail> getOuvrageById(@PathVariable final long ouvrageId)
+	public ResponseEntity<OuvrageDto> getOuvrageById(@PathVariable final long ouvrageId)
 	{
 		try
 		{
-			 Ouvrage ouvrage = ouvrageService.consulterOuvrage(ouvrageId);
-			 return new ResponseEntity<OuvrageDetail>(ouvrageAssembler.createDto(ouvrage), HttpStatus.OK);
+			 OuvrageDto ouvrageDto = ouvrageService.consulterOuvrage(ouvrageId);
+			 return new ResponseEntity<OuvrageDto>(ouvrageDto, HttpStatus.OK);
 		}
-		catch (NullOrEmptyArgumentException | RequiredAndNotFoundException e)
+		catch (EntityNotFoundException e)
 		{
-			return new ResponseEntity<OuvrageDetail>(HttpStatus.NOT_FOUND);
+			return new ResponseEntity<OuvrageDto>(HttpStatus.NOT_FOUND);
 		}
+	}
+	
+	@GetMapping(value = "/ouvrages")
+	public ResponseEntity<Collection<OuvrageDto>> rechercherOuvrages(@RequestBody final OuvrageDto ouvrageDto, 
+			@RequestParam(required = false, defaultValue = "0") int page)
+	{
+		return new ResponseEntity<Collection<OuvrageDto>>(HttpStatus.NOT_FOUND);
 	}
 }
