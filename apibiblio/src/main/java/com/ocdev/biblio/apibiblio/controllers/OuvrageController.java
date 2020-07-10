05cc7bca7eb9a59e4ac6b65c44e9e1a538c21f85
Christophe <christophe.orsini@gmail.com>
2020-07-10 18:06:42 +0200

    modification gestion des ouvrages

--- apibiblio/src/main/java/com/ocdev/biblio/apibiblio/controllers/OuvrageController.java
+++ apibiblio/src/main/java/com/ocdev/biblio/apibiblio/controllers/OuvrageController.java
@@ -21,6 +21,11 @@ import com.ocdev.biblio.apibiblio.errors.AlreadyExistsException;
 import com.ocdev.biblio.apibiblio.errors.EntityNotFoundException;
 import com.ocdev.biblio.apibiblio.services.OuvrageService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/api/v1/*")
 @Validated
@@ -28,6 +33,11 @@ public class OuvrageController
 {
 	@Autowired private OuvrageService ouvrageService;
 	
+	@ApiOperation(value = "Ajout d'un ouvrage", notes = "Ajout d'un nouvel ouvrage")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "L'ouvrage est correctement créé"),
+			@ApiResponse(code = 409, message = "Un ouvrage avec le même titre existe déjà")
+			})
 	@PostMapping(value ="/ouvrages")
 	public ResponseEntity<OuvrageDto> ajouterOuvrage(@Valid @RequestBody final OuvrageDto ouvrageDto) throws AlreadyExistsException
 	{
@@ -35,13 +45,23 @@ public class OuvrageController
 		return new ResponseEntity<OuvrageDto>(result, HttpStatus.CREATED);
 	}
 	
+	@ApiOperation(value = "Consultation d'un ouvrage", notes = "Obtenir les détails d'un ouvrage")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "L'ouvrage trouvé est retourné dans le corps de la réponse"),
+			@ApiResponse(code = 404, message = "L'ouvrage avec cet ID n'existe pas")
+			})
 	@GetMapping(value = "/ouvrages/{ouvrageId}")
-	public ResponseEntity<OuvrageDto> getOuvrageById(@PathVariable @Min(1) final long ouvrageId) throws EntityNotFoundException
+	public ResponseEntity<OuvrageDto> getOuvrageById(@ApiParam(value="ID de l'ouvrage", required = true, example = "1")
+			@PathVariable @Min(1) final long ouvrageId) throws EntityNotFoundException
 	{
 		OuvrageDto ouvrageDto = ouvrageService.consulterOuvrage(ouvrageId);
 		return ResponseEntity.ok(ouvrageDto);
 	}
 	
+	@ApiOperation(value = "Recherche d'ouvrages", notes = "Obtenir une liste d'ouvrages correspondant à plusieurs critères de recherche")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "La liste des ouvrages est retourné dans le corps de la réponse")
+			})
 	@GetMapping(value = "/ouvrages")
 	public ResponseEntity<Collection<OuvrageDto>> rechercherOuvrages(@RequestBody final OuvrageDto ouvrageDto, 
 			@RequestParam(required = false, defaultValue = "0") int page)
--- apibiblio/src/main/java/com/ocdev/biblio/apibiblio/dto/OuvrageDto.java
+++ apibiblio/src/main/java/com/ocdev/biblio/apibiblio/dto/OuvrageDto.java
@@ -3,20 +3,31 @@ package com.ocdev.biblio.apibiblio.dto;
 import java.io.Serializable;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel(value = "OuvrageDto", description = "Modèle DTO pour la classe Ouvrage")
 public class OuvrageDto implements Serializable
 {
+	@ApiModelProperty(position = 1, required = false, value = "ID de l'ouvrage attribuée par le système", allowEmptyValue = true)
 	private Long id;
+	@ApiModelProperty(position = 2, required = true, value = "Titre de l'ouvrage")
 	@NotBlank(message="Le titre est obligatoire")
 	private String titre;
+	@ApiModelProperty(position = 3, required = false, value = "Résumé de l'ouvrage")
 	private String resume;
+	@ApiModelProperty(position = 4, required = true, value = "Auteur de l'ouvrage")
 	@NotBlank(message="L'auteur est obligatoire")
 	private String auteur;
-	@NotBlank(message="L'année de parution est obligatoire")
+	@ApiModelProperty(position = 5, required = true, value = "Année de parution de l'ouvrage")
+	@Positive(message="L'année de parution est obligatoire")
 	private int anneeEdition;
+	@ApiModelProperty(position = 6, required = false, value = "Nombre d'exemplaires disponibles pour prêt")
 	@PositiveOrZero(message = "Le nombre d'exemplaires ne peut pas être négatif")
 	private int nbreExemplaire;
+	@ApiModelProperty(position = 7, required = true, value = "Thème (catégorie) de l'ouvrage")
 	@NotBlank(message="Le thème est obligatoire")
 	private String theme;
 
