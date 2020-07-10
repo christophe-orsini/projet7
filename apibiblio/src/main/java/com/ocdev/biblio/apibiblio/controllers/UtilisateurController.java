921cb74116ab6fdaefe9fee1518ce1b7e1d0072a
Christophe <christophe.orsini@gmail.com>
2020-07-10 18:07:00 +0200

    modification gestion des utilisteurs

--- apibiblio/src/main/java/com/ocdev/biblio/apibiblio/controllers/UtilisateurController.java
+++ apibiblio/src/main/java/com/ocdev/biblio/apibiblio/controllers/UtilisateurController.java
@@ -2,6 +2,7 @@ package com.ocdev.biblio.apibiblio.controllers;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ocdev.biblio.apibiblio.dto.UtilisateurDto;
 import com.ocdev.biblio.apibiblio.errors.AlreadyExistsException;
 import com.ocdev.biblio.apibiblio.services.UtilisateurService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/v1/*")
@@ -17,10 +21,15 @@ public class UtilisateurController
 {
 	@Autowired UtilisateurService utilisateurService;
 	
+	@ApiOperation(value = "Inscription d'un utilisateur", notes = "Inscription d'un nouvel utilisateur de l'application")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "L'utilisateur est correctement inscrit"),
+			@ApiResponse(code = 409, message = "Un utilisateur avec le même email existe déjà")
+			})
 	@PostMapping(value = "/utilisateurs")
 	public ResponseEntity<UtilisateurDto> inscrire(@Valid @RequestBody UtilisateurDto utilisateurDto) throws AlreadyExistsException
 	{
 		UtilisateurDto result = utilisateurService.creer(utilisateurDto);
-		return ResponseEntity.ok(result);
+		return new ResponseEntity<UtilisateurDto>(result, HttpStatus.CREATED);
 	}
 }
--- apibiblio/src/main/java/com/ocdev/biblio/apibiblio/dto/UtilisateurDto.java
+++ apibiblio/src/main/java/com/ocdev/biblio/apibiblio/dto/UtilisateurDto.java
@@ -5,17 +5,26 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "UtilisateurDto", description = "Modèle DTO pour la classe Utilisateur")
 public class UtilisateurDto implements Serializable
 {
+	@ApiModelProperty(position = 1, required = false, value = "ID de l'utilisateur attribuée par le système", allowEmptyValue = true)
 	private Long Id;
+	@ApiModelProperty(position = 2, required = true, value = "Adresse email qui sert de login", example = "email@domain.tld")
 	@NotBlank(message="L'email est obligatoire")
 	@Email(message="L'adresse email n'est pas bien formée")
 	private String email; // login
+	@ApiModelProperty(position = 3, required = true, value = "Mot de passe avec au moins 4 caractères", example = "1234")
 	@NotBlank(message="Le mot de passe est obligatoire")
 	@Size(min = 4, message = "Le mot de passe doit avoir au moins 4 caratères")
 	private String password;
+	@ApiModelProperty(position = 4, required = true, value = "Nom de famille", example = "MonNom")
 	@NotBlank(message="Le nom est obligatoire")
 	private String nom;
+	@ApiModelProperty(position = 5, required = false, value = "Prénom", example = "MonPrénom")
 	private String prenom;
 	
 	public Long getId()
