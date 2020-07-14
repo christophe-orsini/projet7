104016ffa8f67b4e161f0858fa0650ed42ba9c54
Christophe <christophe.orsini@gmail.com>
2020-07-14 10:31:01 +0200

    nettoyage

--- apibiblio/src/main/java/com/ocdev/biblio/apibiblio/controllers/OuvrageController.java
+++ apibiblio/src/main/java/com/ocdev/biblio/apibiblio/controllers/OuvrageController.java
@@ -1,10 +1,8 @@
 package com.ocdev.biblio.apibiblio.controllers;
 
 import java.util.Collection;
-
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.ocdev.biblio.apibiblio.criterias.OuvrageCriteria;
 import com.ocdev.biblio.apibiblio.dto.OuvrageCreateDto;
-import com.ocdev.biblio.apibiblio.dto.OuvrageDto;
 import com.ocdev.biblio.apibiblio.entities.Ouvrage;
 import com.ocdev.biblio.apibiblio.errors.AlreadyExistsException;
 import com.ocdev.biblio.apibiblio.errors.EntityNotFoundException;
 import com.ocdev.biblio.apibiblio.services.OuvrageService;
-
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
