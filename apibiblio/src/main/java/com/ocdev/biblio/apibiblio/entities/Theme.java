2c7ed9d627c7cbc6e49d2c7b1ed8ea840267d6ae
Christophe <christophe.orsini@gmail.com>
2020-07-23 17:27:38 +0200

    correction specifications

--- apibiblio/src/main/java/com/ocdev/biblio/apibiblio/entities/Theme.java
+++ apibiblio/src/main/java/com/ocdev/biblio/apibiblio/entities/Theme.java
@@ -1,15 +1,11 @@
 package com.ocdev.biblio.apibiblio.entities;
 
 import java.io.Serializable;
-import java.util.Collection;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * Classe modèle pour un thème
