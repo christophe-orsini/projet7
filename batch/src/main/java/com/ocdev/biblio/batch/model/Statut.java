792cd9d8efa2fa1f6eb2871769a4e9edf91e30ed
Christophe <christophe.orsini@gmail.com>
2020-08-08 15:45:52 +0200

    Changement nom de package

--- batch/src/main/java/com/ocdev/biblio/batch/model/Statut.java
+++ batch/src/main/java/com/ocdev/biblio/batch/model/Statut.java
@@ -1,4 +1,4 @@
-package com.ocdev.biblio.batch.objects;
+package com.ocdev.biblio.batch.model;
 
 /**
 * Enum pour le statut d'un prêt.
--- batch/src/main/java/com/ocdev/biblio/batch/model/Utilisateur.java
+++ batch/src/main/java/com/ocdev/biblio/batch/model/Utilisateur.java
@@ -1,4 +1,4 @@
-package com.ocdev.biblio.batch.objects;
+package com.ocdev.biblio.batch.model;
 
 import java.io.Serializable;
 import java.util.Collection;
